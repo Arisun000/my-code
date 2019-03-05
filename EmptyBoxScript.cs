93599466b37b5b570486a06f381c7d914c8e4e0b
arisun000 <48049422+Arisun000@users.noreply.github.com>
2019-03-05 18:19:28 +0900

    アイテムボックスのコード
    良く分かってない

---
+++ EmptyBoxScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyBoxScript : MonoBehaviour
+{
+	public GameObject emptybox;
+
+    public void SetItemBoxUI (bool judge)
+    {
+    	if(judge == true)
+    	{
+		emptybox.SetActive(true);
+    	gameObject.SetActive(false);
+		}
+    }
+}
